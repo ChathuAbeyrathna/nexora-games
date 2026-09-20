8b6cb295d7e01055ce97366de786c361c0cd9f6e
ChathuAbeyrathna <abeyrathnamrcd.21@uom.lk>
2026-09-20 16:10:52 +0530

    new build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
+
+  base: command === 'build' ? '/nexora-games/' : '/',
+
   server: {
     port: 5173,
   },
-});
+}));
